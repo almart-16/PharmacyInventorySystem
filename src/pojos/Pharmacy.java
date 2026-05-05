4c0b4bc1d5971e5b51e3c70d274965e89017d5cd
Alex Martinez Fernandez <alex.martinezfernandez@usp.ceu.es>
2026-05-05 16:30:17 +0200

    Photo in Pharmacy and ConnectionManager

--- src/pojos/Pharmacy.java
+++ src/pojos/Pharmacy.java
@@ -7,63 +7,38 @@ public class Pharmacy {
     private String phone;
     private String registrationNumber;
     private int municipalityId;
-
+    private byte[] photo;
     
     public Pharmacy() {
     }
 
-    public Pharmacy(String id, String address, String phone, String registrationNumber, int municipalityId) {
+    public Pharmacy(String id, String address, String phone, String registrationNumber, int municipalityId,byte[] photo ) {
         this.id = id;
         this.address = address;
         this.phone = phone;
         this.registrationNumber = registrationNumber;
         this.municipalityId = municipalityId;
+        this.photo = photo;
     }
 
-    public String getId() {
-        return id;
-    }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
 
-    public String getAddress() {
-        return address;
-    }
+    public String getRegistrationNumber() { return registrationNumber; }
+    public void setRegistrationNumber(String registrationNumber) { this.registrationNumber = registrationNumber; }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public int getMunicipalityId() { return municipalityId; }
+    public void setMunicipalityId(int municipalityId) { this.municipalityId = municipalityId; }
 
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-
-    public int getMunicipalityId() {
-        return municipalityId;
-    }
-
-    public void setMunicipalityId(int municipalityId) {
-        this.municipalityId = municipalityId;
-    }
-
+    public byte[] getPhoto() { return photo; }
+    public void setPhoto(byte[] photo) { this.photo = photo; }
+    
     @Override
     public String toString() {
         return "Pharmacy{" +
