261c9f145c709943c55396c117ea8c9ddb4da297
paolaamorr <paola.amorromero@usp.ceu.es>
2026-05-13 10:45:24 +0200

    JPA manager pero tengo duda

--- src/jpa/JPAManager.java
+++ src/jpa/JPAManager.java
@@ -2,4 +2,33 @@ package jpa;
 
 public class JPAManager {
 
+    private JPAUserManager userManager;
+    private JPARoleManager roleManager;
+
+    public JPAManager() {
+
+        userManager = new JPAUserManager();
+        roleManager = new JPARoleManager();
+    }
+
+    public JPAUserManager getUserManager() {
+
+        return userManager;
+    }
+
+    public JPARoleManager getRoleManager() {
+
+        return roleManager;
+    }
+
+    public void disconnect() {
+
+        if(userManager != null) {
+            userManager.disconnect();
+        }
+
+        if(roleManager != null) {
+            roleManager.disconnect();
+        }
+    }
 }
